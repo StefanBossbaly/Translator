401bfc7855b10077736aadd0ef65b2593f84199b
Stefan Bossbaly <Sbossb@gmail.com>
2013-04-12 12:54:18 -0300

    Update runtime.c

--- src/runtime.c
+++ src/runtime.c
@@ -22,3 +22,49 @@ inline int remainder(int num1, int num2)
 {
     return num1 % num2;
 }
+
+inline int eq(int condition1, int condition2)
+{
+    return (condition1 == condition2);
+}
+
+inline int neq(int condition1, int condition2)
+{
+    return (condition1 != condition2);
+}
+
+inline int lt(int condition1, int condition2)
+{
+    return (condition1 < condition2);
+}
+
+inline int lte(int condition1, int condition2)
+{
+    return (condition1 <= condition2);
+}
+
+inline int gt(int condition1, int condition2)
+{
+    return (condition1 > condition2);
+}
+
+inline int gte(int condition1, int condition2)
+{
+    return (condition1 >= condition2);
+}
+
+//Logical Operations
+inline int not(int condition)
+{
+    return !(condition);
+}
+
+inline int and(int condition1, int condition2)
+{
+    return (condition1 && condition2);
+}
+
+inline int or(int condition1, int condition2)
+{
+    return (condition1 || condition2);
+}
